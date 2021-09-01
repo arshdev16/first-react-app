d5d1a94d63483934082b5dd51acdbcb6383e8d03
arshdev16 <arshisha2516@gmail.com>
2021-09-01 12:23:50 +0530

    restore

--- src/About.js
+++ src/About.js
@@ -4,11 +4,7 @@ export default function Navbar(props) {
 
   return(
     <div>
-<<<<<<< HEAD
      about me
-=======
-    not even close babayyyyy technoblade never diess
->>>>>>> e07a6b2f20bcb633a6dd24ed601aa73211ad725e
     </div>
     )
 }
